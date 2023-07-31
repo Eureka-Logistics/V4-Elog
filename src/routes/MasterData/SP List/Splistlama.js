289d079a014e05e938c0ba18510261beb2deed76
mhrdwan <futuremhr@gmail.com>
2023-07-31 21:08:56 +0700

    tidak mengambil detail lagi di page sp

--- src/routes/MasterData/SP List/Splistlama.js
+++ src/routes/MasterData/SP List/Splistlama.js
@@ -54,23 +54,20 @@ function SPListlama() {
   // const [Pagginations, setPagginations] = useState(1)
   const history = useHistory();
 
-  const dataapi = async (page = 1, pageSize = 10) => {
+  const ApiDataAwal = async (page = 1, pageSize = 10) => {
     try {
-      setLoading(true)
-      const isi = await axios.get(
-        `${Baseurl}sp/get-SP-all?limit=${pageSize}&page=${page}&keyword=${search}&statusSP=&customerId=${CustumerValue}&cabang=${CariCabangValue}&sales=${CariSalesValue}&buId=${CariBu}`,
+      const data = await axios.get(`${Baseurl}sp/get-SP-all?limit=${pageSize}&page=${page}&keyword=${search}&statusSP=&customerId=${CustumerValue}&cabang=${CariCabangValue}&sales=${CariSalesValue}&buId=${CariBu}`,
         {
           headers: {
             "Content-Type": "application/json",
             Authorization: localStorage.getItem("token"),
           },
         }
-      );
-
-      const isidata = isi.data.data.order;
+      )
+      const isidata = data.data.data.order;
       setPagination({
-        currentPage: isi.data.data.currentPage,
-        totalPage: isi.data.data.totalPage,
+        currentPage: data.data.data.currentPage,
+        totalPage: data.data.data.totalPage,
       });
 
       setIsiData(isidata);
@@ -87,302 +84,243 @@ function SPListlama() {
           setTimeout(() => {
             window.location.reload()
           }, 2000);
-          // history.push('/signin');
         }
-      } else {
-        console.error(error);
       }
     }
-  };
-  useEffect((page) => {
-    dataapi();
-    setSPFilter()
-  }, [search, CustumerValue, CariCabangValue, CariSalesValue, CariBu]);
-  console.log(`ini dari spstore`, SPFilter);
-
-  useEffect(() => {
-    if (isiData && isiData.length > 0) {
-      isiData.forEach((item) => {
-        getDestinationData(item.idmp);
-      });
-    }
-  }, [isiData, CustumerValue, CariSalesValue, CariBu]);
-
-  const getDestinationData = async (idmp) => {
-    const isi = await axios.get(`${Baseurl}sp/get-SP-detail?idmp=${idmp}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: localStorage.getItem("token"),
-      },
-    });
-
-    const isis = isi.data.data.map((item) => ({
-      idmp: idmp,
-      kendaraan: item?.kendaraan,
-      pickupAddress: item?.pickupAddress,
-      perusahaan: item?.perusahaan,
-      destination: item?.destination,
-      via: item?.via,
-      item: item?.item,
-      qty: item?.qty,
-      service: item?.service,
-      pickupDate: item?.pickupDate,
-    }));
-
-    setDestinationData((prevData) => [...prevData, ...isis]);
-  };
-
-  const [combinedData, setCombinedData] = useState([]);
-
-  useEffect(() => {
-    if (isiData.length > 0 && destinationData.length > 0) {
-      const combined = isiData.map((isiItem) => {
-        const destItem = destinationData.find(
-          (destinationItem) => destinationItem.idmp === isiItem.idmp
-        );
-
-        if (destItem) {
-          return {
-            ...isiItem,
-            destination: destItem.destination,
-            ...isiItem,
-            kendaraan: destItem.kendaraan,
-          };
-        }
-
-        return isiItem;
-      });
-
-      setCombinedData(combined);
-    }
-  }, [isiData, destinationData]);
-  const columns = [
-    {
-      name: "No",
-      selector: (row) => row?.no,
-      width: "80px",
-      wrap: true,
-    },
-    {
-      name: "SP ID",
-      selector: (row) => row?.sp,
-      width: "150px",
-      wrap: true,
-    },
-    {
-      name: "Perusahaan",
-      selector: (row) => row?.perusahaan,
-      width: "150px",
-      wrap: true,
-    },
-    {
-      name: "Marketing",
-      selector: (row) => row?.salesName,
-      width: "100px",
-      wrap: true,
-    },
-    {
-      name: "Service",
-      selector: (row) => row?.service,
-      width: "100px",
-      wrap: true,
-    },
-    {
-      name: "Vehicle",
-      selector: (row) => row?.kendaraan,
-      width: "100px",
-      wrap: true,
-    },
-    {
-      name: "Pickup Date",
-      selector: (row) => {
-        let date = new Date(row?.pickupDate);
-        let year = date.getFullYear();
-        let month = (1 + date.getMonth()).toString().padStart(2, '0');
-        let day = date.getDate().toString().padStart(2, '0');
-
-        return (
-          <Tag color="green">{`${year}-${month}-${day}`}</Tag> // return dalam format yyyy-mm-dd
-        );
-      },
-      width: "120px",
-      wrap: true,
-    },
-
-    {
-      name: "Destination",
-      selector: (row) => row?.destination,
-      width: "150px",
-      wrap: true,
-    },
-
-    // {
-    //   name: "Tgl Approved/Decline",
-    //   selector: (row) => {
-    //     const dateApproveOps = row?.dateApproveOps;
-    //     const isValidDate = !isNaN(new Date(dateApproveOps));
-
-    //     const data = isValidDate ? dateApproveOps : "-";
-    //     return data
-    //   },
-    // },
-    {
-      name: "Akunting",
-      selector: (row) => {
-        const tanggal = row.dateApproveAct
-        return row?.approveAct === "Y" ? (
-          <Tag color="green">Approved <br /> {tanggal}</Tag>
-        ) : (row?.approveAct === "N" && tanggal === "Invalid date") ? (
-          <Tag color="yellow">Waiting <br /> {tanggal ? "-" : tanggal}</Tag>
-        ) : (row?.approveAct === "N" && tanggal !== "Invalid date") ?
-          (
-            <Tag color="red">Diverted <br /> {tanggal}</Tag>
-          ) : <Tag color="blue">Pass <br /> {tanggal}</Tag>
-      },
-      width: "170px",
-    },
-    {
-      name: "Operasional",
-      selector: (row) => {
-        const dateApproveOps = row?.dateApproveOps;
-        const isValidDate = !isNaN(new Date(dateApproveOps));
-        const data = isValidDate ? dateApproveOps : "-";
-        if (row?.approveOps === "Y") {
-          return <Tag color="green">Approved <br /> {data}</Tag>;
-        } else if (row?.approveOps === "P") {
-          return <Tag color="blue">Pass <br /> {data}</Tag>;
-        } else if (!isValidDate) {
-          return <Tag color="yellow">Waiting <br /> {data}</Tag>;
-        } else {
-          return <Tag color="red">Diverted <br /> {data}</Tag>;
-        }
-      },
-      width: "170px",
-    },
-
-    {
-      name: "Purchasing",
-      selector: (row) => {
-        const date = row?.dateApprovePurch
-        return (
-          <>
-            {(row.approvePurch === "Y" && date != null) ? (
-              <Tag color="green">Approved <br /> {date}</Tag>
-            ) : (
-              (row.approvePurch === "N" && date === "Invalid date") ? (
-                <Tag color="yellow">Waiting <br /> {date ? "-" : date}</Tag>
-              ) : (
-                (row.approvePurch === "N" && date != "Invalid date") ? (
-                  <Tag color="red">Diverted <br /> {date}</Tag>
-                ) : (
-                  <Tag color="blue">Pass <br /> {date}</Tag>
-                )
-              )
-            )}
-          </>
-        )
-      },
-      width: "180px",
-    },
-    // {
-    //   name: "Detail",
-    //   selector: (row) => <><Button size="sm" onClick={() => buttonarahin(row.idmp)}>Detail</Button></>,
-    //   width: "170px",
-    // },
-  ];
-
-  const RowClick = (row) => {
-    history.push(`/masterdata/splistdetailakunting/${row.idmp}`);
   }
-  const buttonarahin = (idmp) => {
-    // history.push(`/masterdata/detailsp/${idmp}`);
-    history.push(`/masterdata/splistdetailakunting/${idmp}`);
-  };
-
-  const handlePageChange = async (page, pageSize) => {
-    dataapi(page, pageSize)
-    // setPagginations(page)
-    // setPagination({ ...pagination, currentPage: page });
-    // await dataapi(page, search);
-  };
-
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
+   
+  useEffect((page) => {
+      setSPFilter()
+      ApiDataAwal()
+    }, [search, CustumerValue, CariCabangValue, CariSalesValue, CariBu]);
 
 
+    const columns = [
+      {
+        name: "No",
+        selector: (row) => row?.no,
+        width: "80px",
+        wrap: true,
+      },
+      {
+        name: "SP ID",
+        selector: (row) => row?.sp,
+        width: "150px",
+        wrap: true,
+      },
+      {
+        name: "Perusahaan",
+        selector: (row) => row?.perusahaan,
+        width: "150px",
+        wrap: true,
+      },
+      {
+        name: "Marketing",
+        selector: (row) => row?.salesName,
+        width: "100px",
+        wrap: true,
+      },
+      {
+        name: "Service",
+        selector: (row) => row?.service,
+        width: "100px",
+        wrap: true,
+      },
+      {
+        name: "Vehicle",
+        selector: (row) => row?.kendaraan,
+        width: "100px",
+        wrap: true,
+      },
+      {
+        name: "Pickup Date",
+        selector: (row) => {
+          let date = new Date(row?.pickupDate);
+          let year = date.getFullYear();
+          let month = (1 + date.getMonth()).toString().padStart(2, '0');
+          let day = date.getDate().toString().padStart(2, '0');
 
-  return (
-    <div>
-      <Card>
-        <Row>
-          <h5>Sp List</h5>
-          <Col>
-            {/* <h1>SP List</h1> */}
-            <Row>
-              <Col sm={2}>
-                <Form.Group controlId="search">
-                  <Select options={CariCustomerOptions} showSearch optionFilterProp="label" onChange={(e) => setCustumerValue(e)} placeholder="Cari Customer" style={{ width: "100%" }} />
-                </Form.Group>
-              </Col>
-              <Col sm={2}>
-                <Form.Group controlId="cabang">
-                  <Select optionFilterProp="label" showSearch options={CariCabangOptions} onChange={(e) => setCariCabangValue(e)} placeholder="Cari Cabang" style={{ width: "100%" }} />
-                </Form.Group>
-              </Col>
-              <Col sm={2}>
-                <Form.Group controlId="sales">
-                  <Select optionFilterProp="label" options={CariSalesOptions} onChange={(e) => setCariSalesValue(e)} showSearch placeholder="Cari Sales" style={{ width: "100%" }} />
-                </Form.Group>
-              </Col>
-              <Col sm={2}>
-                <Form.Group controlId="bu">
-                  <Select options={CariBUOptions} optionFilterProp="label" onChange={(e) => setCariBu(e)} showSearch placeholder="Cari Bu" style={{ width: "100%" }} />
-                </Form.Group>
-              </Col>
-              <Col style={{ display: "flex", justifyContent: "flex-end" }} sm={2}>
-                <Form.Group controlId="search">
-                  <Input
-                    placeholder="Cari No SP"
-                    onChange={handleSearchChange}
-                  />
-                </Form.Group>
-              </Col>
+          return (
+            <Tag color="green">{`${year}-${month}-${day}`}</Tag> // return dalam format yyyy-mm-dd
+          );
+        },
+        width: "120px",
+        wrap: true,
+      },
 
-            </Row>
+      {
+        name: "Destination",
+        selector: (row) => row?.destination,
+        width: "150px",
+        wrap: true,
+      },
 
-            <style>
-              {`
+      // {
+      //   name: "Tgl Approved/Decline",
+      //   selector: (row) => {
+      //     const dateApproveOps = row?.dateApproveOps;
+      //     const isValidDate = !isNaN(new Date(dateApproveOps));
+
+      //     const data = isValidDate ? dateApproveOps : "-";
+      //     return data
+      //   },
+      // },
+      {
+        name: "Akunting",
+        selector: (row) => {
+          const tanggal = row.dateApproveAct
+          return row?.approveAct === "Y" ? (
+            <Tag color="green">Approved <br /> {tanggal}</Tag>
+          ) : (row?.approveAct === "N" && tanggal === "Invalid date") ? (
+            <Tag color="yellow">Waiting <br /> {tanggal ? "-" : tanggal}</Tag>
+          ) : (row?.approveAct === "N" && tanggal !== "Invalid date") ?
+            (
+              <Tag color="red">Diverted <br /> {tanggal}</Tag>
+            ) : <Tag color="blue">Pass <br /> {tanggal}</Tag>
+        },
+        width: "170px",
+      },
+      {
+        name: "Operasional",
+        selector: (row) => {
+          const dateApproveOps = row?.dateApproveOps;
+          const isValidDate = !isNaN(new Date(dateApproveOps));
+          const data = isValidDate ? dateApproveOps : "-";
+          if (row?.approveOps === "Y") {
+            return <Tag color="green">Approved <br /> {data}</Tag>;
+          } else if (row?.approveOps === "P") {
+            return <Tag color="blue">Pass <br /> {data}</Tag>;
+          } else if (!isValidDate) {
+            return <Tag color="yellow">Waiting <br /> {data}</Tag>;
+          } else {
+            return <Tag color="red">Diverted <br /> {data}</Tag>;
+          }
+        },
+        width: "170px",
+      },
+
+      {
+        name: "Purchasing",
+        selector: (row) => {
+          const date = row?.dateApprovePurch
+          return (
+            <>
+              {(row.approvePurch === "Y" && date != null) ? (
+                <Tag color="green">Approved <br /> {date}</Tag>
+              ) : (
+                (row.approvePurch === "N" && date === "Invalid date") ? (
+                  <Tag color="yellow">Waiting <br /> {date ? "-" : date}</Tag>
+                ) : (
+                  (row.approvePurch === "N" && date != "Invalid date") ? (
+                    <Tag color="red">Diverted <br /> {date}</Tag>
+                  ) : (
+                    <Tag color="blue">Pass <br /> {date}</Tag>
+                  )
+                )
+              )}
+            </>
+          )
+        },
+        width: "180px",
+      },
+      // {
+      //   name: "Detail",
+      //   selector: (row) => <><Button size="sm" onClick={() => buttonarahin(row.idmp)}>Detail</Button></>,
+      //   width: "170px",
+      // },
+    ];
+
+    const RowClick = (row) => {
+      history.push(`/masterdata/splistdetailakunting/${row.idmp}`);
+    }
+    const buttonarahin = (idmp) => {
+      // history.push(`/masterdata/detailsp/${idmp}`);
+      history.push(`/masterdata/splistdetailakunting/${idmp}`);
+    };
+
+    const handlePageChange = async (page, pageSize) => {
+      ApiDataAwal(page)
+      // setPagination({ ...pagination, currentPage: page });
+      // await dataapi(page, search);
+    };
+
+    const handleSearchChange = (e) => {
+      setSearch(e.target.value);
+    };
+
+
+
+    return (
+      <div>
+        <Card>
+          <Row>
+            <h5>Sp List</h5>
+            <Col>
+              {/* <h1>SP List</h1> */}
+              <Row>
+                <Col sm={2}>
+                  <Form.Group controlId="search">
+                    <Select options={CariCustomerOptions} showSearch optionFilterProp="label" onChange={(e) => setCustumerValue(e)} placeholder="Cari Customer" style={{ width: "100%" }} />
+                  </Form.Group>
+                </Col>
+                <Col sm={2}>
+                  <Form.Group controlId="cabang">
+                    <Select optionFilterProp="label" showSearch options={CariCabangOptions} onChange={(e) => setCariCabangValue(e)} placeholder="Cari Cabang" style={{ width: "100%" }} />
+                  </Form.Group>
+                </Col>
+                <Col sm={2}>
+                  <Form.Group controlId="sales">
+                    <Select optionFilterProp="label" options={CariSalesOptions} onChange={(e) => setCariSalesValue(e)} showSearch placeholder="Cari Sales" style={{ width: "100%" }} />
+                  </Form.Group>
+                </Col>
+                <Col sm={2}>
+                  <Form.Group controlId="bu">
+                    <Select options={CariBUOptions} optionFilterProp="label" onChange={(e) => setCariBu(e)} showSearch placeholder="Cari Bu" style={{ width: "100%" }} />
+                  </Form.Group>
+                </Col>
+                <Col style={{ display: "flex", justifyContent: "flex-end" }} sm={2}>
+                  <Form.Group controlId="search">
+                    <Input
+                      placeholder="Cari No SP"
+                      onChange={handleSearchChange}
+                    />
+                  </Form.Group>
+                </Col>
+
+              </Row>
+
+              <style>
+                {`
           .rdt_TableBody .rdt_TableRow:hover {
             cursor: pointer;
             background-color: #C7E1FB;
           }
           
         `}
-            </style>
+              </style>
 
-            {(Loading ? (<img src={ElogLoadingGif}></img>) : (
-              <DataTable
-                columns={columns}
-                data={combinedData}
-                onRowClicked={RowClick}
-                className="myCustomTable"
-              />
-            ))}
-            <div className="mt-3 d-flex justify-content-end">
-              <Pagination
-                showSizeChanger
-                onShowSizeChange={handlePageChange}
-                onChange={handlePageChange}
-                defaultCurrent={1}
-                total={pagination.totalPage}
-              />
-            </div>
-          </Col>
-        </Row>
-      </Card>
-    </div>
-  );
-}
+              {(Loading ? (<img src={ElogLoadingGif}></img>) : (
+                <DataTable
+                  columns={columns}
+                  data={isiData}
+                  onRowClicked={RowClick}
+                  className="myCustomTable"
+                />
+              ))}
+              <div className="mt-3 d-flex justify-content-end">
+                <Pagination
+                  showSizeChanger
+                  onShowSizeChange={handlePageChange}
+                  onChange={handlePageChange}
+                  defaultCurrent={1}
+                  total={pagination.totalPage}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Card>
+      </div>
+    );
+  }
 
-export default SPListlama;
+  export default SPListlama;
