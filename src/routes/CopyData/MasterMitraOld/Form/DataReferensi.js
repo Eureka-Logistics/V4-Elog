1184c02d2a8087df41b669a3a8517c55525a03ae
mhrdwan <futuremhr@gmail.com>
2023-09-10 20:17:56 +0700

    ridwan

--- src/routes/CopyData/MasterMitraOld/Form/DataReferensi.js
+++ src/routes/CopyData/MasterMitraOld/Form/DataReferensi.js
@@ -10,114 +10,6 @@ import Baseurl from "../../../../Api/BaseUrl";
 
 function DataReferensi({ mitraId, SemuaDataUntukEdit, setActiveTab }) {
   const id_mitras = mitraId;
-  const [DataKodeMitra, setDataKodeMitra] = useState("");
-  const [DataKodeInisial, setDataKodeInisial] = useState("");
-  const [DataQRKode, setDataQRKode] = useState("");
-  const [DataNamaMitra, setDataNamaMitra] = useState("");
-  const [DataTitle, setDataTitle] = useState("");
-  const [DataJenisUsaha, setDataJenisUsaha] = useState("");
-  const [DataKepemilikan, setDataKepemilikan] = useState("");
-  const [DataJumlahArmada, setDataJumlahArmada] = useState("");
-  const [DataJumlahSDMOperasional, setDataJumlahSDMOperasional] = useState("");
-  const [DataCabang, setDataCabang] = useState("");
-  const [DataJenisKiriman, setDataJenisKiriman] = useState("");
-  const [DataWilayah, setDataWilayah] = useState("");
-  const [DataTujuan, setDataTujuan] = useState("");
-  const [DataTahunAwalKontrak, setDataTahunAwalKontrak] = useState("");
-  const [DataAwalKontrak, setDataAwalKontrak] = useState("");
-  const [DataAkhirKontrak, setDataAkhirKontrak] = useState("");
-  const [DataKontrak, setDataKontrak] = useState("");
-  const [DataTahunBerdiri, setDataTahunBerdiri] = useState("");
-  const [DataNPWPID, setDataNPWPID] = useState("");
-  const [DataNPWPName, setDataNPWPName] = useState("");
-  const [DataNPWPAddress, setDataNPWPAddress] = useState("");
-  const [DataNPWPJalan, setDataNPWPJalan] = useState("");
-  const [DataBlok, setDataBlok] = useState("");
-  const [DataNPWPNomor, setDataNPWPNomor] = useState("");
-  const [DataNPWPRT, setDataNPWPRT] = useState("");
-  const [DataNPWPRW, setDataNPWPRW] = useState("");
-  const [DataNPWKelurahan, setDataNPWKelurahan] = useState("");
-  const [DataNPWPKecamatan, setDataNPWPKecamatan] = useState("");
-  const [DataNPWPKota, setDataNPWPKota] = useState("");
-  const [DataNPWPProvinsi, setDataNPWPProvinsi] = useState("");
-  const [DataNPWPKodePos, setDataNPWPKodePos] = useState("");
-  const [DataISTaxAble, setDataISTaxAble] = useState("");
-  const [DataContactPerson, setDataContactPerson] = useState("");
-  const [DataTelp, setDataTelp] = useState("");
-  const [DataFax, setDataFax] = useState("");
-  const [DataEmail, setDataEmail] = useState("");
-  const [DataAlamat, setDataAlamat] = useState("");
-  const [DataHomePage, setDataHomePage] = useState("");
-  const [DataPembayaran, setDataPembayaran] = useState("");
-  const [DataNamaAkunBank, setDataNamaAkunBank] = useState("");
-  const [DataNamaBankk, setDataNamaBankk] = useState("");
-  const [DataNoRek, setDataNoRek] = useState("");
-  const [DataCurrency, setDataCurrency] = useState("");
-  const [DataDirektur, setDataDirektur] = useState("");
-  const [DataTelepon, setDataTelepon] = useState("");
-  const [DataPICPurchasing, setDataPICPurchasing] = useState("");
-  const [DataStatusUsaha, setDataStatusUsaha] = useState("");
-  const [DataTop, setDataTop] = useState("");
-  const [DataTahunRegister, setDataTahunRegister] = useState("");
-  const [DataMemo, setDataMemo] = useState("");
-  const [DataType, setDataType] = useState("");
-  const [DataPicId, setDataPicId] = useState("");
-  const [selectMitraPIC, setselectMitraPIC] = useState("");
-  const [DataPic, setDataPic] = useState("");
-  const [DataIDPic, setDataIDPic] = useState("");
-  const [PoLegalitas, setPoLegalitas] = useState("");
-  const [KTPLegalitas, setKTPLegalitas] = useState("");
-  const [AktaPendirian, setAktaPendirian] = useState("");
-  const [AktaPerubahanDasar, setAktaPerubahanDasar] = useState("");
-  const [AktaSusunanDireksi, setAktaSusunanDireksi] = useState("");
-  const [SuratDomisili, setSuratDomisili] = useState("");
-  const [NPWPLegalitas, setNPWPLegalitas] = useState("");
-  const [SktLegalitas, setSktLegalitas] = useState("");
-  const [NppkpLegalitas, setNppkpLegalitas] = useState("");
-  const [SiupLegalitas, setSiupLegalitas] = useState("");
-  const [IjinPenidirian, setIjinPendirian] = useState("");
-  const [PPMDLegalitas, setPPMDLegalitas] = useState("");
-  const [IjinUsaha, setIjinUsaha] = useState("");
-  const [TDPLegalitas, setTDPLegalitas] = useState("");
-  const [SuratKuasaLegalitas, setSuratKuasaLegalitas] = useState("");
-  const [LamaBekerjaLegalitas, setLamaBekerjaLegalitas] = useState("");
-  const [JenisKartuKredit, setJenisKartuKredit] = useState("");
-  const [StatusUsaha, setStatusUsaha] = useState("");
-  const [LamaUsaha, setLamaUsaha] = useState("");
-  const [OmsetBulanan, setOmsetBulanan] = useState("");
-  const [AssetTanah, setAssetTanah] = useState("");
-  const [AssetBangunan, setAssetBangunan] = useState("");
-  const [AssetKendaraan, setAssetKendaran] = useState("");
-  const [AssetMesin, setAssetMesin] = useState("");
-  const [Affiliasi, setAffiliasi] = useState("");
-  const [JumlahUnit, setJumlahUnit] = useState("");
-  const [PeriodeSewa, setPeriodeSewa] = useState("");
-  const [NilaiSewa, setNilaiSewa] = useState("");
-  const [NilaiRUU, setNilaiRUU] = useState("");
-  const [QtyMotor, setQtyMotor] = useState("");
-  const [RpMotor, setRpMotor] = useState("");
-  const [QtyGrandMax, setQtyGrandMax] = useState("");
-  const [RpGrandmax, setRpGrandmax] = useState("");
-  const [Qty1300, setQty1300] = useState("");
-  const [Rp1300, setRp1300] = useState("");
-  const [QtyTraga, setQtyTraga] = useState("");
-  const [RpTraga, setRpTraga] = useState("");
-  const [QtyCDE, setQtyCDE] = useState("");
-  const [RpCDE, setRpCDE] = useState("");
-  const [QtyCDD, setQtyCDD] = useState("");
-  const [RpCDD, setRpCDD] = useState("");
-  const [QtyFuso, setQtyFuso] = useState("");
-  const [RpFuso, setRpFuso] = useState("");
-  const [QtyWingbox, setQtyWingbox] = useState("");
-  const [RpWingbox, setRpWingbox] = useState("");
-  const [QtyTrailer20, setQtyTrailer20] = useState("");
-  const [RpTrailer20, setRpTrailer20] = useState("");
-  const [QtyTrailer40, setQtyTrailer40] = useState("");
-  const [RpTrailer40, setRpTrailer40] = useState("");
-  const [BankPenerbit, setBankPenerbit] = useState("");
-  const [LaporanKeuangan, setLaporankeuanga] = useState("");
-  const [DataJenis, setDataJenis] = useState("");
-  const [TypeOfPayment, setTypeOfPayment] = useState("");
   const [datareverensis, setDataReference] = useState({
     akta_pendirian: null,
     akta_perubahan_dasar: null,
@@ -137,7 +29,7 @@ function DataReferensi({ mitraId, SemuaDataUntukEdit, setActiveTab }) {
   });
   const { data, setData } = isiDatamasterMitraDetailZustand();
 
-  console.log(`oper bang datareverensis`, datareverensis.po_legalitas);
+  console.log(`oper bang datareverensis`, datareverensis);
 
   useEffect(() => {
     const datareverensi = async () => {
@@ -174,120 +66,14 @@ function DataReferensi({ mitraId, SemuaDataUntukEdit, setActiveTab }) {
     return <p>Loading...</p>;
   }
 
-  const EditMitra = () => {
-    setActiveTab("keuangan");
-    const data = {
-      id_mitra: mitraId,
-      jenis: DataJenis,
-      kode_mitra: DataKodeMitra,
-      nama_mitra: DataNamaMitra,
-      kode: DataKodeInisial,
-      qrcode: DataQRKode,
-      title: DataTitle,
-      jenis_usaha: DataJenisUsaha,
-      kepemilikan: DataKepemilikan,
-      jumlah_armada: DataJumlahArmada,
-      jumlah_sdm_operasional: DataJumlahSDMOperasional,
-      cabang: DataCabang,
-      jenis_kiriman: DataJenisKiriman,
-      wilayah: DataWilayah,
-      tujuan: DataTujuan,
-      tahun_awal_kontrak: DataTahunAwalKontrak,
-      awal_kontrak: DataAwalKontrak,
-      akhir_kontrak: DataAkhirKontrak,
-      kontrak: DataKontrak,
-      direktur: DataDirektur,
-      tahun_berdiri: DataTahunBerdiri,
-      npwp_id: DataNPWPID,
-      npwp_name: DataNPWPName,
-      npwp_address: DataNPWPAddress,
-      npwp_jalan: DataNPWPJalan,
-      npwp_blok: DataBlok,
-      npwp_nomor: DataNPWPNomor,
-      npwp_rt: DataNPWPRT,
-      npwp_rw: DataNPWPRW,
-      npwp_kelurahan: DataNPWKelurahan,
-      npwp_kecamatan: DataNPWPKecamatan,
-      npwp_kota: DataNPWPKota,
-      npwp_provinsi: DataNPWPProvinsi,
-      npwp_kodepos: DataNPWPKodePos,
-      is_taxable: DataISTaxAble,
-      telepon: DataTelepon,
-      contact_person: DataContactPerson,
-      telp: DataTelp,
-      fax: DataFax,
-      email: DataEmail,
-      alamat: DataAlamat,
-      homepage: DataHomePage,
-      pembayaran: DataPembayaran,
-      nama_bank: DataNamaBankk,
-      nama_akun: DataNamaAkunBank,
-      no_rek: DataNoRek,
-      currency: DataCurrency,
-      status_usaha: DataStatusUsaha,
-      top: DataTop,
-      memo: DataMemo,
-      type: DataType,
-      metode_pembayaran: TypeOfPayment,
-      pic_id: DataPicId,
-      po_legalitas: PoLegalitas,
-      ktp_legalitas: KTPLegalitas,
-      akta_pendirian: AktaPendirian,
-      akta_perubahan_dasar: AktaPerubahanDasar,
-      akta_susunan_direksi: AktaSusunanDireksi,
-      surat_domisili: SuratDomisili,
-      npwp_legalitas: NPWPLegalitas,
-      skt_legalitas: SktLegalitas,
-      nppkp_legalitas: NppkpLegalitas,
-      siup_legalitas: SiupLegalitas,
-      ijin_pendirian: IjinPenidirian,
-      ppmd_legalitas: PPMDLegalitas,
-      ijin_usaha: IjinUsaha,
-      tdp_legalitas: TDPLegalitas,
-      surat_kuasa: SuratKuasaLegalitas,
-      lama_bekerja: LamaBekerjaLegalitas,
-      jenis_kartu_kredit: JenisKartuKredit,
-      bank_penerbit: BankPenerbit,
-      laporan_keuangan: LaporanKeuangan,
-      status_usaha: StatusUsaha,
-      lama_usaha: LamaUsaha,
-      omset_bulanan: OmsetBulanan,
-      asset_tanah: AssetTanah,
-      asset_bangunan: AssetBangunan,
-      asset_kendaraan: AssetKendaraan,
-      asset_mesin: AssetMesin,
-      affiliasi: Affiliasi,
-      jumlah_unit: JumlahUnit,
-      periode_sewa: PeriodeSewa,
-      nilai_sewa: NilaiSewa,
-      nilai_ruu: NilaiRUU,
-      qty_motor: parseInt(QtyMotor),
-      rp_motor: parseInt(RpMotor),
-      qty_grandmax: parseInt(QtyGrandMax),
-      rp_grandmax: parseInt(RpGrandmax),
-      qty_l300: parseInt(Qty1300),
-      rp_l300: parseInt(Rp1300),
-      qty_traga: parseInt(QtyTraga),
-      rp_traga: parseInt(RpTraga),
-      qty_cde: parseInt(QtyCDE),
-      rp_cde: parseInt(RpCDE),
-      qty_cdd: parseInt(QtyCDD),
-      rp_cdd: parseInt(RpCDD),
-      qty_fuso: parseInt(QtyFuso),
-      rp_fuso: parseInt(RpFuso),
-      qty_wingbox: parseInt(QtyWingbox),
-      rp_wingbox: parseInt(RpWingbox),
-      qty_trailer20: parseInt(QtyTrailer20),
-      rp_trailer20: parseInt(QtyTrailer20),
-      qty_trailer40: parseInt(QtyTrailer40),
-      rp_trailer40: parseInt(RpTrailer40),
-    };
-    setData(data);
-  };
 
   const EditMitras = async () => {
+    const datasemua = {
+      ...data,
+      ...datareverensis
+    };
     try {
-      const response = await axios.post(`${Baseurl}mitra/edit-mitra`, data, {
+      const response = await axios.post(`${Baseurl}mitra/edit-mitra`, datasemua, {
         headers: {
           "Content-Type": "application/json",
           Authorization: localStorage.getItem("token"),
