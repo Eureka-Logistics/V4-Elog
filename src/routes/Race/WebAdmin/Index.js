b5aae6bd1170d9b358ae331da53cd6a34afa06c4
mhrdwan <futuremhr@gmail.com>
2023-09-13 22:31:01 +0700

    ridwan malem

--- src/routes/Race/WebAdmin/Index.js
+++ src/routes/Race/WebAdmin/Index.js
@@ -1,4 +1,4 @@
-import { Button, Card, Col } from 'antd'
+import { Button, Card } from 'antd'
 import React from 'react'
 import './style.css';
 import btnLihat from "../../../assets/img/buttonLihat.png"
@@ -6,7 +6,7 @@ import card1 from "../../../assets/img/dashboard race/card1.png"
 import card2 from "../../../assets/img/dashboard race/card2.png"
 import card3 from "../../../assets/img/dashboard race/card3.png"
 import card4 from "../../../assets/img/dashboard race/card4.png"
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 function Index() {
     return (
         <>
@@ -14,7 +14,7 @@ function Index() {
 
                 <div style={{ fontSize: 25, display: "flex", justifyContent: "start", fontWeight: "bold" }}>Welcome to Dashboard!</div>
                 <Row className='mt-3'>
-                    <Col sm={6}>
+                    <Col sm={3}>
                         <Card style={{ backgroundColor: "", height: 160, width: 263, padding: 0, backgroundImage: `url(${card1})` }}>
                             <div style={{ color: "white", marginTop: "-15px", marginLeft: "-10px", fontSize: 15, alignItems: "flex-start", justifyContent: "flex-start", display: "flex", height: "120%" }}>
                                 Surat Perjalanan Kantor
@@ -23,7 +23,7 @@ function Index() {
                             <Button style={{ width: "100%", marginTop: 25 }}><img src={btnLihat}></img> </Button>
                         </Card>
                     </Col>
-                    <Col sm={6}>
+                    <Col sm={3}>
                         <Card style={{ backgroundColor: "", height: 160, width: 263, padding: 0, backgroundImage: `url(${card2})` }}>
                             <div style={{ color: "white", marginTop: "-15px", marginLeft: "-10px", fontSize: 15, alignItems: "flex-start", justifyContent: "flex-start", display: "flex", height: "120%" }}>
                                 SM
@@ -32,7 +32,7 @@ function Index() {
                             <Button style={{ width: "100%", marginTop: 25 }}><img src={btnLihat}></img> </Button>
                         </Card>
                     </Col>
-                    <Col sm={6}>
+                    <Col sm={3}>
                         <Card style={{ backgroundColor: "", height: 160, width: 263, padding: 0, backgroundImage: `url(${card3})` }}>
                             <div style={{ color: "white", marginTop: "-15px", marginLeft: "-10px", fontSize: 15, alignItems: "flex-start", justifyContent: "flex-start", display: "flex", height: "120%" }}>
                                 SPK
@@ -41,7 +41,7 @@ function Index() {
                             <Button style={{ width: "100%", marginTop: 25 }}><img src={btnLihat}></img> </Button>
                         </Card>
                     </Col>
-                    <Col sm={6}>
+                    <Col sm={3}>
                         <Card style={{ backgroundColor: "", height: 160, width: 263, padding: 0, backgroundImage: `url(${card4})` }}>
                             <div style={{ color: "white", marginTop: "-15px", marginLeft: "-10px", fontSize: 15, alignItems: "flex-start", justifyContent: "flex-start", display: "flex", height: "100%" }}>
                                 SO
@@ -52,31 +52,38 @@ function Index() {
                     </Col>
 
                 </Row>
-                <Row>
-                    <Col sm={14} style={{ backgroundColor: "white" }}>
-                        <div >
-                            Pengiriman Anda
-                        </div>
-                    </Col>
-                    <Col sm={10} style={{ backgroundColor: "red", height: 200 }}>
-                        <div >
-                            Pengiriman Anda
-                        </div>
-                    </Col>
-                </Row>
-                <Row className='mt-3'>
-                    <Col sm={7} style={{ backgroundColor: "white" }}>
-                        <div style={{ height: 240, width: 360 }}>
-                            Score Bulanan
-                        </div>
-                    </Col>
-                    <Col sm={7} style={{ backgroundColor: "white" }}>
-                        <div style={{ height: 240, width: 360 }}>
-                            Data Driver
-                        </div>
-                    </Col>
-                </Row>
 
+                <div style={{ position: 'relative' }}>  {/* Position relative disini agar elemen absolut lainnya berpatokan pada elemen ini */}
+                    <Row>
+                        <Col sm={8} style={{ backgroundColor: 'lightblue', margin: '5px', height: '328px' }}>
+                            <div>
+                                Pengiriman Anda
+                            </div>
+                        </Col>
+                        <Col sm={3} style={{ backgroundColor: 'lightcoral', margin: '5px', height: '600px' }}>
+                            <div>
+                                Pengiriman saya
+                            </div>
+                        </Col>
+
+                    {/* Row for Data Driver and Score Bulanan */}
+                    <div style={{ position: 'absolute', top: '350px', }}> {/* Posisi absolut */}
+                        <Row>
+                            <Col sm={4} style={{ backgroundColor: 'lightgreen', margin: '5px', height: '240px' }}>
+                                <div>
+                                    Data Driver
+                                </div>
+                            </Col>
+                            <Col sm={4} style={{ backgroundColor: 'lightyellow', margin: '5px', height: '240px' }}>
+                                <div>
+                                    Score Bulanan
+                                </div>
+                            </Col>
+                    </Row>
+
+                    </div>
+                        </Row>
+                </div>
             </Container>
 
         </>
