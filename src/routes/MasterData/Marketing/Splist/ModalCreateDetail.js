5bb6fb145ee45a64e881cb66119e8f379606ae11
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-10-27 17:58:07 +0700

    ridwan

--- src/routes/MasterData/Marketing/Splist/ModalCreateDetail.js
+++ src/routes/MasterData/Marketing/Splist/ModalCreateDetail.js
@@ -415,8 +415,8 @@ function ModalCreateDetail({ AlamatInvoiceOptions, DetailSemua, idmp, DetailSP, 
             return (HasilTarifNumerik * berat) + totalBiayaTambahan;
         } else if (DetailSemua?.service === 'Charter') {
             return HasilTarifNumerik + totalBiayaTambahan;
-        } else if (DetailSemua?.service?.toLowerCase() === 'retail' || DetailSemua?.service?.toLowerCase() === 'retailer') {
-            return (HasilTarifNumerik * berat) + totalBiayaTambahan;
+        } else if ( formik.values.via === "laut" && DetailSemua?.service?.toLowerCase() === 'retail' || DetailSemua?.service?.toLowerCase() === 'retailer') {
+            return HasilTarifNumerik + totalBiayaTambahan;
         }
     };
 
