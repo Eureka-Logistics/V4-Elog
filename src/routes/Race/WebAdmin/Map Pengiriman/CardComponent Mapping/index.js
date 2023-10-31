731014729834b47db5d744eb2f4920d652fcff8d
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-10-31 16:53:20 +0700

    ridwan

--- src/routes/Race/WebAdmin/Map Pengiriman/CardComponent Mapping/index.js
+++ src/routes/Race/WebAdmin/Map Pengiriman/CardComponent Mapping/index.js
@@ -1,4 +1,4 @@
-import { Button, Card, Checkbox } from 'antd'
+import { Button, Card, Checkbox, Tag } from 'antd'
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import CardMappingStoreRace from '../../../../../zustand/Store/DriverMappingCardRace/MappingStore';
@@ -28,7 +28,7 @@ function CardMapping({ onAdd, DataApi }) {
 
                         </Col>
                         <Col>
-                            <h4 style={{ fontSize: 15, fontWeight: "bold" }}>{item.sp}</h4>
+                            <h4 style={{ fontSize: 15, fontWeight: "bold" }}><Tag color="blue">{item.sp}</Tag></h4>
                         </Col>
                         <Col className='d-flex justify-content-end'>
                             <Button disabled={item.status === "Waiting"} style={{ height: 30, width: 80 }}>
@@ -37,52 +37,52 @@ function CardMapping({ onAdd, DataApi }) {
                         </Col>
                     </Row>
                     <Row className='mt-3'>
-                        <Col sm={7}>
+                        <Col style={{fontWeight:"bold"}}  sm={7}>
                             <p>Pelanggan</p>
                         </Col>
-                        <Col className='d-flex justify-content-start' style={{}} sm={5}>
+                        <Col className='d-flex justify-content-start' style={{fontWeight:"bold"}} sm={5}>
                             <p>Tanggal Pick Up</p>
                         </Col>
                         <Col style={{ marginTop: -15 }} sm={7}>
-                            <p style={{ fontSize: "16px", fontWeight: "bold" }}>{item.customer}</p>
+                            <p style={{ fontSize: "16px", fontWeight: "bold" }}><Tag color="gold">{item.customer}</Tag></p>
                         </Col>
                         <Col style={{ marginTop: -15 }} sm={4}>
-                            <p style={{ fontSize: "16px", fontWeight: "bold" }}>{item.pickupDate}</p>
+                            <p style={{ fontSize: "16px", fontWeight: "bold" }}><Tag color="green">{item.pickupDate}</Tag></p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col sm={3}>
+                        <Col style={{fontWeight:"bold"}}  sm={6}>
                             <p>Penerima</p>
                         </Col>
-                        <Col sm={3} className='d-flex justify-content-start'>
+                        <Col style={{fontWeight:"bold"}}  sm={2} className='d-flex justify-content-start'>
                             <p>ikat</p>
                         </Col>
-                        <Col sm={3} className='d-flex justify-content-start'>
+                        <Col style={{fontWeight:"bold"}}  sm={2} className='d-flex justify-content-start'>
                             <p>koli</p>
                         </Col>
-                        <Col sm={3} className='d-flex justify-content-start' style={{}}>
+                        <Col style={{fontWeight:"bold"}}  sm={2} className='d-flex justify-content-start' >
                             <p>Qty</p>
                         </Col>
-                        <Col sm={3} style={{ marginTop: -15 }}>
-                            <p style={{ fontSize: "12px", fontWeight: "bold" }}>{item.penerima}</p>
+                        <Col sm={6} style={{ marginTop: -15 }}>
+                            <p style={{ fontSize: "12px", fontWeight: "bold" }}><Tag color='red'>{item.penerima}</Tag></p>
                         </Col>
-                        <Col sm={3} className='d-flex justify-content-start' style={{ marginTop: -15 }}>
-                            <p style={{ fontSize: "12px", fontWeight: "bold" }}>{item.ikat}</p>
+                        <Col sm={2} className='d-flex justify-content-start' style={{ marginTop: -15 }}>
+                            <p style={{ fontSize: "12px", fontWeight: "bold" }}><Tag color='teal'>{item.ikat}</Tag></p>
                         </Col>
-                        <Col sm={3} className='d-flex justify-content-start' style={{ marginTop: -15 }}>
-                            <p style={{ fontSize: "16px", fontWeight: "bold" }}>{item.koli}</p>
+                        <Col sm={2} className='d-flex justify-content-start' style={{ marginTop: -15 }}>
+                            <p style={{ fontSize: "16px", fontWeight: "bold" }}><Tag color="teal">{item.koli}</Tag></p>
                         </Col>
-                        <Col sm={3} className='d-flex justify-content-start' style={{ marginTop: -15 }}>
-                            <p style={{ fontSize: "16px", fontWeight: "bold" }}>{item.qty}</p>
+                        <Col sm={2} className='d-flex justify-content-start' style={{ marginTop: -15 }}>
+                            <p style={{ fontSize: "16px", fontWeight: "bold" }}><Tag color="teal">{item.qty}</Tag></p>
                         </Col>
 
                     </Row>
                     <Row>
-                        <Col sm={3} className='d-flex justify-content-start'>
+                        <Col style={{fontWeight:"bold"}}  sm={3} className='d-flex justify-content-start'>
                             <p>Tujuan Muatan</p>
                         </Col>
                         <Col className='d-flex justify-content-start' style={{}}>
-                            <p style={{ fontSize: "12px", fontWeight: "bold" }}>{item.tujuan}</p>
+                            <Tag color='purple' style={{ fontSize: "12px", fontWeight: "bold" , overflow : "hidden"}}>{item.tujuan}</Tag>
                         </Col>
                     </Row>
                 </Card>
