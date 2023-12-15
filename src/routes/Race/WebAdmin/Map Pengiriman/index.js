aa632a7507f7d9fcb4f0a815466d59c91c3b52d7
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-12-15 09:43:56 +0700

    ridwan

--- src/routes/Race/WebAdmin/Map Pengiriman/index.js
+++ src/routes/Race/WebAdmin/Map Pengiriman/index.js
@@ -93,7 +93,7 @@ function MapPengiriman() {
 
     const kirimPesanWhatsApp = async (item) => {
         try {
-            const response = await axios.post(`https://botwa.mhridwan.com/send-ke-driver`, [item]);
+            const response = await axios.post(`https://34.30.16.30:3001/send-ke-driver`, [item]);
             console.log(response);
             {
                 response && response?.data?.forEach((item) => (
