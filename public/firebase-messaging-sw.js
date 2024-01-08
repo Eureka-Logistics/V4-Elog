3cd92d9229685f3d3e020b933ed9083321804bcf
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2024-01-08 09:01:03 +0700

    ridwan

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging/sw";
-
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
@@ -18,3 +19,7 @@ const firebaseApp = initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = getMessaging(firebaseApp);
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
--- src/routes/Race/WebAdmin/Index.js
+++ src/routes/Race/WebAdmin/Index.js
@@ -95,33 +95,31 @@ function Index() {
                 {setdata?.issue?.[0]?.day[0]} -  {setdata?.issue?.[0]?.month[0]} - {setdata?.issue?.[0]?.year[0]}
             </div>
             <div className='area'>
-                {setdata && setdata?.area.map((item, index) => (
-                    <div key={index} style={{ display: 'flex', marginBottom: '20px' }}>
-                        <div style={{ flex: 1, paddingRight: '10px' }}>
-                            <div>Name: {item?.name?.[0]?._}</div>
-                        </div>
-                        <div style={{ flex: 2 }}>
-                            {item?.parameter?.map((param, paramIndex) => {
-                                if (param?.$?.id === 'tmax') {
-                                    return (
-                                        <>
-                                            <div key={paramIndex}>
-                                                <div> {param?.$?.description}</div>
-                                            </div>
-                                            {param.timerange.map((time, index) => (
-                                                <>
-                                                    <div>{moment(time?.$?.day, "YYYYMMDD").format('DD-MM-YYYY')}</div>
-                                                    <div>{time?.value?.[0]?._}{time?.value?.[0]?.$?.unit}</div>
-                                                </>
-                                            ))}
-                                        </>
-                                    );
-                                }
-                            })}
-                        </div>
+            {setdata && setdata?.area.map((item, index) => (
+                <div key={index} style={{ display: 'flex', marginBottom: '20px', alignItems: 'flex-start' }}>
+                    <div style={{ flex: 1, paddingRight: '10px' }}>
+                        <div>Name: {item?.name?.[0]?._}</div>
                     </div>
-                ))}
-            </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', flex: 2 }}>
+                        {item?.parameter?.map((param, paramIndex) => {
+                            if (param?.$?.id === 'tmax') {
+                                return (
+                                    <div key={paramIndex} style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                                        {param.timerange.map((time, timeIndex) => (
+                                            <div key={timeIndex} style={{ marginRight: '50px' }}>
+                                                <div>{moment(time?.$?.day, "YYYYMMDD").format('DD-MM-YYYY')}</div>
+                                                <div>{time?.value?.[0]?._}{time?.value?.[0]?.$?.unit}</div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                );
+                            }
+                            return null;
+                        })}
+                    </div>
+                </div>
+            ))}
+        </div>
 
 
 
